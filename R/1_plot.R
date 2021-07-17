7f14dcddf1722b485302ccfe9b656d2c6b91e1e6
Rolf Britten <rolfbritten@gmail.com>
2021-07-17 12:40:04 +0100

    create and output a light theme and a dark theme version

--- R/1_plot.R
+++ R/1_plot.R
@@ -6,9 +6,24 @@ ridge_df <- config$rides %>%
   map_dfr( get_plot_data )
 
 
-#max(ridge_df$elevation) #603.5
-
 # set the `scale` to determine how much overlap there is among the plots
-ggplot(ridge_df, aes(x=distance_percent, y=ride, height = elevation, group = ride)) +
-  ggridges::geom_density_ridges(fill="white",scale=4,stat="identity",size=1.5)+
-  theme_light()
+base_plot <- ggplot(ridge_df, aes(x=distance_percent, y=ride, height = elevation, group = ride))
+
+# light theme
+light_plot <- base_plot +
+  ggridges::geom_density_ridges(fill="white",scale=config$plot_vars$scale,stat="identity",size=config$plot_vars$line_size)+
+  theme(
+    panel.grid.major = element_blank(), panel.grid.minor = element_blank(), # no tick lines
+    panel.background = element_rect(fill = "white",colour = "white")
+  )
+
+# dark theme
+dark_plot <- ggplot(ridge_df, aes(x=distance_percent, y=ride, height = elevation, group = ride)) +
+  ggridges::geom_density_ridges(colour = "white", fill = "black",scale=config$plot_vars$scale,stat="identity",size=config$plot_vars$line_size)+
+  theme(
+    panel.grid.major = element_blank(), panel.grid.minor = element_blank(), # no tick lines
+    panel.background = element_rect(fill = "black",colour = "black")
+  )
+
+ggsave(filename = 'light_plot.jpeg', plot = light_plot, width = 21, height =9, units = "cm")
+ggsave(filename = 'dark_plot.jpeg', plot = dark_plot, width = 21, height =9, units = "cm")
